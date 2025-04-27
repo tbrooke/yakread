76c19a1e887c99dfd19d109e7df8378ce4bc2d65
Jacob O'Bryant <git@jacobobryant.com>
2025-04-27 03:28:03 -0600

    Finish discover recommendation scratch code

---
+++ java/stuff/MyFunction1.java
@@ -0,0 +1,29 @@
+package stuff;
+
+import scala.Function1;
+import scala.Tuple2;
+import org.apache.spark.mllib.recommendation.Rating;
+import java.io.Serializable;
+import java.util.List;
+import clojure.lang.PersistentVector;
+
+public class MyFunction1 implements Function1<Tuple2<Integer, List<Rating>>, Object>, Serializable {
+    @Override
+    public Object apply(Tuple2 tuple) {
+        Integer itemIndex = (Integer) tuple._1;
+        Rating[] ratings = (Rating[]) tuple._2;
+
+        double avg = 0.0;
+        if (ratings != null && ratings.length > 0) {
+            double sum = 0.0;
+            for (Rating rating : ratings) {
+                sum += rating.rating();
+            }
+            avg = sum / ratings.length;
+        }
+
+        return PersistentVector.create(itemIndex, avg);
+    }
+}
+
+// javac --release 17 -cp $(clj -Spath) -d target/classes java/stuff/MyFunction1.java -Xlint:-options
--- src/com/yakread/model/recommend.clj
+++ src/com/yakread/model/recommend.clj
@@ -4,12 +4,18 @@
             [com.yakread.lib.pathom :as lib.pathom]
             [com.yakread.lib.error :as lib.error]
             [com.yakread.lib.core :as lib.core]
+            [com.yakread.lib.user-item :as lib.usit]
             [com.wsscode.pathom3.interface.async.eql :as p.a.eql]
             [com.wsscode.pathom3.connect.operation :as pco :refer [defresolver ?]]
             [com.wsscode.pathom3.interface.eql :as p.eql]
             [com.wsscode.pathom3.connect.indexes :as pci]
             [xtdb.api :as xt]
-            [lambdaisland.uri :as uri]))
+            [lambdaisland.uri :as uri])
+  (:import [org.apache.spark.api.java JavaSparkContext]
+           [org.apache.spark.mllib.recommendation Rating ALS MatrixFactorizationModel]
+           [scala Tuple2 Function1]
+           [scala.reflect ClassTag$]
+           [stuff MyFunction1]))
 
 (def n-ads 1)
 (def n-sub-bookmark-recs 22)
@@ -228,6 +234,196 @@
                bookmark-recs
                for-you-recs]})
 
+(defn median [xs]
+  (first (take (/ (count xs) 2) xs)))
+
+(defn take-rand [xs]
+  (take (max 1 (* (gen/double) (count xs))) xs))
+
+(defn foo [tuple]
+  (let [index (._1 ^Tuple2 tuple)
+        ratings (._2 ^Tuple2 tuple)
+        avg (/ (apply + (mapv (fn [^Rating rating]
+                                (.rating rating))
+                              ratings))
+               (count ratings))]
+    [index avg]))
+
+(def bar
+  (proxy [Function1 java.io.Serializable] []
+    (apply [tuple]
+      (foo tuple))))
+
+(defn -main [ctx]
+  (with-open [spark (JavaSparkContext. "local[*]" "yakread")]
+    (.setCheckpointDir spark "storage/spark-checkpoint")
+    (let [{:keys [biff/db] :as ctx} (biff/merge-context ctx)
+          user-id (biff/lookup-id db :user/email "jacob@thesample.ai")
+          item-id->url (time
+                        (into {}
+                              (q db
+                                 '{:find [item2 url]
+                                   :where [[usit :user-item/item item1]
+                                           [usit :user-item/favorited-at]
+                                           [item1 :item/url url]
+                                           [item2 :item/url url]]})))
+          _ (def item-id->url item-id->url)
+          all-usits (time
+                     (doall
+                      (q db
+                         '{:find (pull usit [*])
+                           :in [[item ...]]
+                           :where [[usit :user-item/item item]]}
+                         (keys item-id->url))))
+          _ (def all-usits all-usits)
+          skip-usits (time
+                      (q db
+                         '{:find [user item (count skip)]
+                           :keys [user-item/user user-item/item user-item/n-skips]
+                           :in [[item ...]]
+                           :where [[skip :skip/items item]
+                                   [skip :skip/user user]]}
+                         (keys item-id->url)))
+          _ (def skip-usits skip-usits)
+          usit-key (juxt :user-item/user :user-item/item)
+          user+item->skip-usit (into {}
+                                     (map (juxt usit-key identity))
+                                     skip-usits)
+          all-usits (->> (concat (for [usit all-usits]
+                                   (merge usit (user+item->skip-usit (usit-key usit))))
+                                 skip-usits)
+                         (lib.core/distinct-by usit-key)
+                         (mapv #(assoc % :user-item/url (item-id->url (:user-item/item %)))))
+          my-usits (filterv #(= (:user-item/user %) user-id)
+                            all-usits)
+
+          url-allowed? (constantly true)
+
+          ;; build a CF model
+          ;;   base it on item url, not item id
+          ;; get list of unread items
+          ;; we want to look at:
+          ;; - CF predicted rating
+          ;; - freshness (maybe?)
+          ;; - popularity (bias toward long tail)
+
+          ;; sort items by n-usits + skips unique by user, then take a percentage of the list
+          ;; sort remaining items by last-liked-at, then take a percentage of the list
+          ;; 90% of the time: take the max item by [- n-subject-skips, predicted rating, random]
+          ;;   do the (rerank 0.1) thing, or similar
+          ;;   for digests, include digests in n-seen
+          ;; 10% of the time: pick a random item
+
+          [[index->url url->index]
+           [index->user user->index]]
+          (for [k [:user-item/url :user-item/user]
+                :let [index->x (->> all-usits
+                                    (mapv k)
+                                    distinct
+                                    (map-indexed vector)
+                                    (into {}))]]
+            [index->x (into {} (map (fn [[k v]] [v k])) index->x)])
+
+          ratings (time
+                   (->> all-usits
+                        (mapv (fn [usit]
+                                (let [score (cond
+                                              (:user-item/favorited-at usit) 1
+                                              (:user-item/disliked-at usit) 0
+                                              (:user-item/reported-at usit) 0
+                                              (:user-item/viewed-at usit) 0.75
+                                              (:user-item/skipped-at usit) (max 0 (- 0.5 (* 0.1 (:user-item/n-skips usit 0))))
+                                              (:user-item/bookmarked-at usit) 0.6
+                                              ;; Shouldn't ever happen; just being defensive.
+                                              :else 0.5)]
+                                  (Rating. (int (user->index (:user-item/user usit)))
+                                           (int (url->index (:user-item/url usit)))
+                                           (double score)))))
+                        (.parallelize spark)
+                        (.rdd)
+                        (.cache)))
+          _ (prn (count all-usits) (count index->user) (count index->url))
+          rank 10
+          iterations 20
+          lambda 0.1
+          alpha 0.05
+          _ (println "training")
+          model (time (ALS/trainImplicit ratings rank iterations lambda alpha))
+          _ (println "done training")
+
+          url->n-usits (update-vals (group-by :user-item/url all-usits)
+                                    count)
+          url->last-liked (update-vals (group-by :user-item/url all-usits)
+                                       (fn [usits]
+                                         (->> (keep :user-item/favorited-at usits)
+                                              (apply max-key inst-ms))))
+          url->n-cur-user-skips (into {}
+                                      (keep (fn [{:user-item/keys [url n-skips]}]
+                                              (when n-skips
+                                                [url n-skips])))
+                                      my-usits)
+          _ (println "computing baselines")
+          baselines (-> (into {} (.. model
+                                     (recommendUsersForProducts (count user->index))
+                                     (map (stuff.MyFunction1.)
+
+                                          #_(reify Function1
+                                              (apply [this tuple]
+                                                (let [url (index->url (._1 ^Tuple2 tuple))
+                                                      ratings (._2 ^Tuple2 tuple)
+                                                      avg (/ (apply + (mapv (fn [^Rating rating]
+                                                                              (.rating rating))
+                                                                            ratings))
+                                                             (count ratings))]
+                                                  [url avg])))
+                                          (.apply ClassTag$/MODULE$ clojure.lang.PersistentVector))
+                                     (collect)))
+                        (update-keys index->url))
+          _ (biff/pprint (take 5 baselines))
+
+          median-rating (median (sort (vals baselines)))
+          _ (println "computing url->score")
+          url->score (time
+                      (if-some [user-idx (user->index user-id)]
+                        (->> (.recommendProducts model user-idx (count index->url))
+                             (into {} (map (fn [^Rating rating]
+                                             [(index->url (.product rating))
+                                              (.rating rating)]))))
+                        baselines))
+          read-urls (time
+                     (into #{}
+                           (keep (fn [{:user-item/keys [url] :as usit}]
+                                   (when (lib.usit/read? usit)
+                                     url)))
+                           my-usits))
+
+          urls (->> (vals index->url)
+                    (remove read-urls)
+                    (sort-by (juxt url->n-usits (comp - inst-ms url->last-liked))))
+          _ (prn (count urls) "candidates")
+          urls (time
+                (first
+                 (reduce (fn [[selected candidates] _]
+                           (let [selection
+                                 (if (< (gen/double) 0.1)
+                                   (gen/rand-nth candidates)
+                                   (->> candidates
+                                        take-rand
+                                        (sort-by (comp - inst-ms url->last-liked))
+                                        take-rand
+                                        gen/shuffle
+                                        (sort-by (fn [url]
+                                                   [(get url->n-cur-user-skips url 0)
+                                                    (- (get url->score url median-rating))]))
+                                        (rerank 0.25)
+                                        first))]
+                             [(conj selected selection)
+                              (filterv (complement #{selection}) candidates)]))
+                         [[] urls]
+                         (range 5))))
+          ]
+    urls)))
+
 (comment
 
   (let [ctx (biff/merge-context @com.yakread/system)
@@ -243,5 +439,16 @@
          :user/for-you-recs
          (take 10)
          time))
+
+
+  (with-open [spark (JavaSparkContext. "local[*]" "yakread")]
+    :ok)
+
+  (def spark (JavaSparkContext. "local[*]" "yakread"))
+
+
+  (-main @com.yakread/system)
+
+
 
   )
