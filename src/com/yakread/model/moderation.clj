c805f98e9b5dc6ca1787446f7e95c1f2c9e58bcb
Jacob O'Bryant <git@jacobobryant.com>
2025-08-17 08:51:12 -0600

    Improve moderation page load speed

--- src/com/yakread/model/moderation.clj
+++ src/com/yakread/model/moderation.clj
@@ -9,25 +9,43 @@
                  :admin.moderation/approved
                  :admin.moderation/blocked
                  :admin.moderation/ingest-failed]}
-  (let [items (q db
-                 '{:find [direct-item (count usit)]
-                   :keys [item/id item.moderation/likes]
-                   :in [direct]
-                   :order-by [[(count usit) :desc]]
-                   :where [[usit :user-item/item any-item]
-                           [usit :user-item/favorited-at]
-                           [any-item :item/url url]
-                           [direct-item :item/url url]
-                           [direct-item :item/doc-type direct]
-                           (not [direct-item :item.direct/candidate-status])]}
-                 :item/direct)
+  (let [direct-items (q db
+                        '{:find (pull item [*])
+                          :in [direct]
+                          :where [[item :item/doc-type direct]]}
+                        :item/direct)
+        url->direct-item (into {} (map (juxt :item/url identity)) direct-items)
+        liked-items (q db
+                       '{:find [item (count usit)]
+                         :keys [item/id item.moderation/likes]
+                         :order-by [[(count usit) :desc]]
+                         :where [[usit :user-item/item item]
+                                 [usit :user-item/favorited-at]]})
+        item->url (into {}
+                        (q db
+                           '{:find [item url]
+                             :in [[item ...]]
+                             :where [[item :item/url url]]}
+                           (mapv :item/id liked-items)))
+        direct-item-id->likes (->> liked-items
+                                   (mapv (fn [{:keys [item/id item.moderation/likes]}]
+                                           (when-some [id (-> id item->url url->direct-item :xt/id)]
+                                             {id likes})))
+                                   (apply merge-with +))
+        liked-direct-items (->> (into []
+                                      (comp (map #(assoc % :item.moderation/likes (direct-item-id->likes (:xt/id %))))
+                                            (filter :item.moderation/likes)
+                                            (remove :item.direct/candidate-status))
+                                      direct-items)
+                                (sort-by :item.moderation/likes >)
+                                vec)
         statuses (into {} (q db
                              '{:find [status (count item)]
                                :where [[item :item.direct/candidate-status status]]}))]
-    {:admin.moderation/remaining (count items)
+    {:admin.moderation/remaining (count liked-direct-items)
      :admin.moderation/approved (get statuses :approved 0)
      :admin.moderation/blocked (get statuses :blocked 0)
      :admin.moderation/ingest-failed (get statuses :ingest-failed 0)
-     :admin.moderation/next-batch (vec (take 50 items))}))
+     :admin.moderation/next-batch (vec (take 50 liked-direct-items))}))
 
 (def module {:resolvers [next-batch]})
